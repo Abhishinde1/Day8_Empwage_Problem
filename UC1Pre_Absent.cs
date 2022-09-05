f37d767647a1683999748d9657b4aa5887c78a93
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-05 23:55:10 +0530

    PresentAbsent Added

--- UC1Pre_Absent.cs
+++ UC1Pre_Absent.cs
@@ -8,5 +8,18 @@ namespace basic
 {
     internal class UC1Pre_Absent
     {
+        public static void EmployeeAttendance()
+        {
+            Random obj = new Random();
+            int num = obj.Next(0, 2);
+            if (num == 1)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
     }
 }
