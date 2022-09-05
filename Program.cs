01d270ef85cea9e9ffe5652a7351a4b6ed766bb8
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-05 23:46:43 +0530

    Welcome to EmpWage

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using static basic.UC1Pre_Absent;
+
+namespace basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Empwage Program");
+            Console.WriteLine("Choose opion \n 1.Check emp abs or pres");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1: UC1Pre_Absent.EmployeeAttendance(); break;
+            }
+        }
+    }
+    }
