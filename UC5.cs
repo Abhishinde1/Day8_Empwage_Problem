6f1de280df51ea5dc438f1a0bfe05d5ca7562f82
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-06 16:24:58 +0530

    Month empwage

--- UC5.cs
+++ UC5.cs
@@ -8,5 +8,37 @@ namespace basic
 {
     internal class UC5
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+
+        public static void MonthWage()
+        {
+            int empHrs = 0; int empWage = 0; int totalEmpWage = 0;
+
+            for (int i = 0; i < NUM_OF_WORKING_DAYS; i++)
+            {
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HR;
+                Console.WriteLine("Employe wage is " + empWage);
+                totalEmpWage += empWage;
+
+            }
+            Console.WriteLine("Total Emp Wage is " + totalEmpWage);
+        }
     }
 }
