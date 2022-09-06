3e0c406eb585c74e7334dd786867493a9efd94ab
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-06 16:32:14 +0530

    Welcome to Employee Wage Computation

--- Program.cs
+++ Program.cs
@@ -7,12 +7,17 @@ namespace basic
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Empwage Program");
-            Console.WriteLine("Choose opion \n 1.Check emp abs or pres");
+            Console.WriteLine("Choose opion \n 1.Check emp abs or pres \n 2.Calculate daily wage\n 3. Part time wage \n 4. Emp Wage Using Case");
 
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1: UC1Pre_Absent.EmployeeAttendance(); break;
+                case 2: UC2DailyWage.CalculateDailyWage(); break;
+                case 3: UC3.CalculateDailyPartTimeWage(); break;
+                case 4: UC4.CalculateDailyWageUsingCase(); break;
+                case 5: UC5.MonthWage(); break;
+                case 6: UC6.CalculateWage(); break;
             }
         }
     }
